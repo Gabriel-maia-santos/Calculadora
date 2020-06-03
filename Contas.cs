a6e72a4018583c01c2cfe5e96897e2226d7a1098
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-06-03 15:46:58 -0300

    Primeiro Commit

---
+++ Contas.cs
@@ -0,0 +1,10 @@
+namespace Calculadora
+{
+    public class Contas
+    {
+        public string  num1;
+        public string num2;
+        public string num3;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program : Contas
+    {
+        static void Main(string[] args)
+        {
+        
+        string num2 = "A";
+
+        Console.WriteLine("Digite o primeiro número");
+        Double num1 = Double.Parse(Console.ReadLine());
+
+        //+ = soma ; - =  subtração ; * = multiplicação; / = divisão
+        Console.WriteLine("escolha um: - + * /");
+        num2 = Console.ReadLine();
+
+
+        Console.WriteLine("Digite o segundo número");
+        Double num3 = Double.Parse(Console.ReadLine());
+
+
+        if(num2 == "-"){
+            double resultado1 = num1 - num3;
+            Console.Clear();
+            Console.WriteLine("Resposta: " + resultado1);
+        }else{
+        if(num2 == "+"){
+            double resultado2 = num1 + num3;
+            Console.Clear();
+            Console.WriteLine("Resposta: " + resultado2);
+        }else{
+        if(num2 == "*"){
+            double resultado3 = num1 * num3;
+            Console.Clear();
+            Console.WriteLine("Resposta: " + resultado3);
+
+        }else{
+        if(num2 == "/"){
+            double resultado4 = num1 / num3;
+            Console.Clear();
+            Console.WriteLine("Resposta: " + resultado4);
+
+        }
+        }
+        }
+        }
+
+        
+         
+
+        
+
+        
+
+
+        }
+    }
+}
